f9b1e835765d442b325c44f9a300cdd7dbc6d77f
cdang730 <doradang730@gmail.com>
2025-11-08 13:37:58 -0500

    back to no supabase for backend

--- backend/main.py
+++ backend/main.py
@@ -4,18 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import random
-from supabase import create_client, Client
-import os
-import random
-from dotenv import load_dotenv
 
-# Load .env
-load_dotenv()
-
-# Connect to Supabase
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 mood_to_tea = {
     "happy": ["Green Tea", "White Tea", "Jasmine Tea"],
@@ -38,7 +27,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Example data model for POST requests
 class MoodRequest(BaseModel):
     mood: str
 
@@ -47,14 +35,16 @@ class MoodRequest(BaseModel):
 def get_tea(mood_request: MoodRequest):
     mood = mood_request.mood.lower()
     teas = mood_to_tea.get(mood)
+    
     if teas: 
         recommended_tea = random.choice(teas)
     else:
         # if not return defalt tea
         recommended_tea = "Jasmine Tea"
-    data = {"mood": mood, "recommended_tea": recommended_tea}
-    response = supabase.table("moods").insert(data).execute()
-    return {"mood": mood, "recommended_tea": recommended_tea, "message": f"For your {mood} mood, try {recommended_tea}!", "saved": bool(response.data)}
+
+    return {"mood": mood, 
+            "recommended_tea": recommended_tea,
+            "message": f"For your {mood} mood, try {recommended_tea}!"}
 
 
 
